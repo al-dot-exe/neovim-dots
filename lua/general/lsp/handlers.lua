16c18728933b0e2fee761c2797a9b96ab4958865
Al.exe <alEXE-tech@protonmail.com>
2022-10-26 14:41:27 -0700

    Fixed deprecation warning issue within handlers.lua

--- lua/general/lsp/handlers.lua
+++ lua/general/lsp/handlers.lua
@@ -102,6 +102,10 @@ M.on_attach = function(client, bufnr)
 	lsp_highlight_document(client)
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+-- !!! Made changes to update_capabilities due to deprecation warning
+-- will erase if no more errors occur
+
+-- M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 return M
