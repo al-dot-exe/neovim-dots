b1e3fa0cd74c518d94013c61a7f72c8d6de7a247
Alex Burton <alex.burton@medcrypt.co>
2024-07-29 17:17:27 -0700

    Removed luarocks issue

--- lua/general/plugins.lua
+++ lua/general/plugins.lua
@@ -125,8 +125,6 @@ return packer.startup(function(use)
   use({
   "andythigpen/nvim-coverage",
   requires = "nvim-lua/plenary.nvim",
-  -- Optional: needed for PHP when using the cobertura parser
-  rocks = { 'lua-xmlreader' },
   config = function()
     require("coverage").setup()
   end,
